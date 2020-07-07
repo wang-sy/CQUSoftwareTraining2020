3ea72e173f9d868823cc102ca8eb7894f8b25c5f
victor-leee <763858741@qq.com>
2020-07-08 01:25:57 +0800

    code quick fix

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import Axios from 'axios'
-Vue.config.productionTip = false
-=======
+
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
@@ -16,8 +6,6 @@ import 'element-ui/lib/theme-chalk/index.css'
 import Axios from 'axios'
 import VueAreaLinkage from 'vue-area-linkage';
 import router from './router/index';
->>>>>>> a7bfab21a565f2e08188dc529b362e74e2bf0902
-
 let globalAxiosInstance = Axios.create({
   baseURL:'http://www.wangsaiyu.cn:20001'
 });
@@ -31,17 +19,6 @@ Vue.prototype.$parseError = (error) => {
 }
 
 Vue.config.productionTip = false
-<<<<<<< HEAD
-
-Vue.use(ElementUI);
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
-=======
 Vue.use(VueAreaLinkage);
 Vue.use(ElementUI);
 /* eslint-disable no-new */
@@ -49,4 +26,3 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app")
->>>>>>> a7bfab21a565f2e08188dc529b362e74e2bf0902
