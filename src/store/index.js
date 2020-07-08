88318c81e36c22de53372f518f7406fef1191907
victor-leee <763858741@qq.com>
2020-07-08 22:47:48 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cartItems:[
+      {
+        sku_id:'xxx',
+        number:1
+      }
+    ]
   },
   mutations: {
+    addCartItem(sku_id, number) {
+      state.cartItems.push({sku_id, number});
+    },
+    deleteCardItem(sku_id) {
+      state.cartItems.splice(state.cartItems.findIndex(item => item.sku_id === sku_id), 1);
+    }
   },
   actions: {
   },
