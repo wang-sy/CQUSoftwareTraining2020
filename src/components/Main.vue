840f6bc018eba169fe2c19fa0f31ef698e4c33d6
FingerLeader <FingerLeader@gmail.com>
2020-07-09 12:12:16 +0800

    主页更新了卡片显示

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -17,35 +17,37 @@
         </el-header>
 
         <el-main>
-        <el-table :data="showData = showAllData ? tableData : tableData.filter(item => item.class_name === showClass)">
-          <el-table-column prop="spu_figure_url" label="    " min-width="40%" >
-            <template slot-scope="scope">
-              <el-image :lazy='true' :src="scope.row.spu_figure_url"
-                style="width: 50px; height: 50px" @click="goToGoods(scope.row.spu_id)">
+<!--        <el-table :data="showData = showAllData ? tableData : tableData.filter(item => item.class_name === showClass)">-->
+<!--          <el-table-column prop="spu_figure_url" label="    " min-width="40%" >-->
+<!--            <template slot-scope="scope">-->
+<!--              <el-image :lazy='true' :src="scope.row.spu_figure_url"-->
+<!--                style="width: 50px; height: 50px" @click="goToGoods(scope.row.spu_id)">-->
+<!--              </el-image>-->
+<!--            </template>-->
+<!--          </el-table-column>-->
+<!--          <el-table-column prop="spu_name" label="名称" min-width="40%">-->
+<!--          </el-table-column>-->
+<!--          <el-table-column prop="spu_lowest_price" label="价格">-->
+<!--          </el-table-column>-->
+<!--        </el-table>-->
+          <el-row v-for="(page, index) of pages ":key="index" >
+            <el-col :span="4" v-for="(item, innerindex) of page" :key="item.spu_id" :offset="innerindex > 0 ? 2 : 1">
+              <el-card :body-style="{ padding: '10px' }">
+              <el-image :lazy='true' :src="item.spu_figure_url"
+                style="width: 280px; height: 220px;"
+                @click.native="goToGoods(item.spu_id)"
+                class="image">
               </el-image>
-            </template>
-          </el-table-column>
-          <el-table-column prop="spu_name" label="名称" min-width="40%">
-          </el-table-column>
-          <el-table-column prop="spu_lowest_price" label="价格">
-          </el-table-column>
-        </el-table>
-          <!--        <el-row :data="tableData">-->
-          <!--  <el-col :span="8" :property=tableData.spu_figure_url >-->
-          <!--    <el-card :body-style="{ padding: '0px' }" >-->
-          <!--           <template slot-scope="scope">-->
-          <!--              <el-image :lazy='true' :src="scope.spu_figure_url"-->
-          <!--                style="width: 50px; height: 50px"></el-image>-->
-          <!--           </template>-->
-          <!--      <div style="padding: 14px;">-->
-          <!--        <span :property="tableData.spu_name"></span>-->
-          <!--        <div class="bottom clearfix">-->
-          <!--          <el-button type="text" class="button">操作按钮</el-button>-->
-          <!--        </div>-->
-          <!--      </div>-->
-          <!--    </el-card>-->
-          <!--  </el-col>-->
-          <!--</el-row>-->
+                <div style="padding: 14px;">
+                  <span>{{item.spu_name}}</span>
+                  <div class="bottom clearfix">
+                    <span class="showPrice" >￥{{item.spu_lowest_price}}</span>
+                    <el-button type="text" @click.native="goToGoods(item.spu_id)" class="button" >详情</el-button>
+                  </div>
+                </div>
+              </el-card>
+            </el-col>
+          </el-row>
         </el-main>
       </el-container>
     </el-container>
@@ -67,6 +69,7 @@
     data() {
       // 展示商品的的表单
       let showClass = /(.*?)/;
+      let tableData = [];
       return {
         showClass,
         showAllData: true,
@@ -127,7 +130,20 @@
           }
         });
       }
-
+    },
+    computed: {
+      pages() {
+        const pages = []
+        this.tableData.forEach((item, index) => {
+          const page = Math.floor(index / 4)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+        })
+        console.log(pages)
+        return pages
+      }
     }
   }
 </script>
@@ -143,11 +159,44 @@
     color: #333;
   }
   .el-card {
-    min-width: 380px;
-    margin-right: 20px;
+    min-width: 250px;
+    margin-right: 4px;
+    width: 300px;
+    height: 400px;
     transition: all .5s;
   }
   .el-card:hover{
     margin-top: -5px;
+  }
+  .showPrice{
+    color: red;
+    font-size: 30px;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+    font-size: 18px;
+    bottom: auto;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+
+  .clearfix:after {
+      clear: both
   }
 </style>
